e4c8d557c493f656f866d38335252df8a8ba7457
J <jpham26@gmail.com>
2024-07-05 14:41:40 -0700

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import '../src/assets/css/style.css'
 import '../src/assets/css/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
