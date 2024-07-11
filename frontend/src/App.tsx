5c9561f8d2bbb96463bb9c5ae2b7fd98b36c8d80
J <jpham26@gmail.com>
2024-07-11 11:06:02 -0700

    Add ClientCacheProvider to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { ClientCacheProvider } from './context/ClientCacheContext';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
 import DetailsPage from './pages/DetailsPage';
@@ -8,13 +9,15 @@ import Layout from './components/Layout';
 const App: React.FC = () => {
   return (
     <HelmetProvider>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route element={<Layout />}>
-          <Route path='/search' element={<SearchPage />} />
-          <Route path='/details/:tmdbId/:mediaType' element={<DetailsPage />} />
-        </Route>
-      </Routes>
+      <ClientCacheProvider>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route element={<Layout />}>
+            <Route path='/search' element={<SearchPage />} />
+            <Route path='/details/:tmdbId/:mediaType' element={<DetailsPage />} />
+          </Route>
+        </Routes>
+      </ClientCacheProvider>
     </HelmetProvider>
   );
 };
