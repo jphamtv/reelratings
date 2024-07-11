63d685c7da3f6859f3b7d1c9575c1f06680a1e3e
J <jpham26@gmail.com>
2024-07-11 11:53:03 -0700

    Update HomePage cache functionality

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
+import { useClientCache } from '../hooks/useClientCache';
 import { fetchTrendingMovies } from '../services/api';
 import SearchBar from '../components/SearchBar';
 import Footer from '../components/Footer';
 import reelRatingsLogo from '../assets/img/reelratings_logo_yellow.svg';
 import styles from './HomePage.module.css';
 
-interface Movie {
+interface TrendingMovie {
   tmdb_id: number;
   title: string;
   year: string;
@@ -15,19 +16,36 @@ interface Movie {
   poster_img: string;
 }
 
+interface TrendingMoviesResponse {
+  results: TrendingMovie[];
+}
+
 const HomePage: React.FC = () => {
-  const [trendingMovies, setTrendingMovies] = useState<Movie[]>([]);
+  const [trendingMovies, setTrendingMovies] = useState<TrendingMovie[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const { getItem, setItem } = useClientCache();
 
   useEffect(() => {
     const fetchMovies = async () => {
+      const cacheKey = 'trending_movies';
+      const cachedMovies = getItem<TrendingMoviesResponse>(cacheKey);
+
+      if (cachedMovies && Array.isArray(cachedMovies.results)) {
+        setTrendingMovies(cachedMovies.results);
+        return;
+      }
+
       try {
         setLoading(true);
         setError(false);
-        const response = await fetchTrendingMovies();
-        console.log(response)
-        setTrendingMovies(response.results);
+        const movies = await fetchTrendingMovies();
+        if (movies && Array.isArray(movies.results)) {
+        setTrendingMovies(movies.results);
+        setItem(cacheKey, movies)
+        } else {
+          throw new Error('Invalid reponse format');
+        }
       } catch (err) {
         console.error("Error fetching movies:", err);
         setError(true);
@@ -37,7 +55,7 @@ const HomePage: React.FC = () => {
     };
 
     fetchMovies();
-   }, []);
+   }, [getItem, setItem]);
 
   if (loading) {
     return <div className={styles.loading}>Loading...</div>;
@@ -64,9 +82,9 @@ const HomePage: React.FC = () => {
           <div className={styles.moviesContainer}>
             <h3 className={styles.title}>Popular Movies This Week</h3>
             <div className={styles.movieGrid}>
-            {trendingMovies.map((result) => (
-              <Link to={`/details/${result.tmdb_id}/${result.media_type}`} key={result.tmdb_id}>
-                <img src={result.poster_img} alt={result.title} className={styles.posterImage}/>
+            {trendingMovies.map((movie) => (
+              <Link to={`/details/${movie.tmdb_id}/${movie.media_type}`} key={movie.tmdb_id}>
+                <img src={movie.poster_img} alt={movie.title} className={styles.posterImage}/>
               </Link>
             ))}
             </div>
